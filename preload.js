45c9717b2abccef7743a05d756cd2ee8ee1c6867
Alexander Henriquez <ahvdesign@gmail.com>
2023-02-14 19:08:18 -0500

    UI overhaul

---
+++ preload.js
@@ -0,0 +1,11 @@
+// TODO: Update this to handle slot info updates
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
