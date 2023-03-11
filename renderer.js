0be2c92c8948031d60a22c516eb7379dc3717a1e
Alexander Henriquez <ahvdesign@gmail.com>
2023-03-11 02:01:49 -0500

    discarded sanity, ui is wired up

--- renderer.js
+++ renderer.js
@@ -194,6 +194,53 @@ function updateUI() {
       }
     });
   });
+
+  const characterNameListSubmit = document.getElementById(
+    "characterNameListSubmit"
+  );
+  characterNameListSubmit.addEventListener("click", function (e) {
+    updateCharacterList();
+  });
+  const character1 = document.getElementById("character1");
+  const char1 = document.getElementById("char1");
+  character1.innerHTML = data.activeSlot.characters.one;
+  char1.innerHTML = data.activeSlot.characters.one;
+  const character2 = document.getElementById("character2");
+  const char2 = document.getElementById("char2");
+  character2.innerHTML = data.activeSlot.characters.two;
+  char2.innerHTML = data.activeSlot.characters.two;
+  const character3 = document.getElementById("character3");
+  const char3 = document.getElementById("char3");
+  character3.innerHTML = data.activeSlot.characters.three;
+  char3.innerHTML = data.activeSlot.characters.three;
+  const character4 = document.getElementById("character4");
+  const char4 = document.getElementById("char4");
+  character4.innerHTML = data.activeSlot.characters.four;
+  char4.innerHTML = data.activeSlot.characters.four;
+  const character5 = document.getElementById("character5");
+  const char5 = document.getElementById("char5");
+  character5.innerHTML = data.activeSlot.characters.five;
+  char5.innerHTML = data.activeSlot.characters.five;
+  const character6 = document.getElementById("character6");
+  const char6 = document.getElementById("char6");
+  character6.innerHTML = data.activeSlot.characters.six;
+  char6.innerHTML = data.activeSlot.characters.six;
+  const character7 = document.getElementById("character7");
+  const char7 = document.getElementById("char7");
+  character7.innerHTML = data.activeSlot.characters.seven;
+  char7.innerHTML = data.activeSlot.characters.seven;
+  const character8 = document.getElementById("character8");
+  const char8 = document.getElementById("char8");
+  character8.innerHTML = data.activeSlot.characters.eight;
+  char8.innerHTML = data.activeSlot.characters.eight;
+  const character9 = document.getElementById("character9");
+  const char9 = document.getElementById("char9");
+  character9.innerHTML = data.activeSlot.characters.nine;
+  char9.innerHTML = data.activeSlot.characters.nine;
+  const character10 = document.getElementById("character10");
+  const char10 = document.getElementById("char10");
+  character10.innerHTML = data.activeSlot.characters.ten;
+  char10.innerHTML = data.activeSlot.characters.ten;
 }
 
 function initializeUI() {
@@ -296,6 +343,31 @@ function updateActiveSlotName(name) {
 
       writeJsonFileSync(JSON.stringify(data));
       setActiveSlot(slot.id.toString());
+    }
+  });
+}
+
+function updateCharacterList() {
+  const data = JSON.parse(readJsonFileSync());
+  const characterNameInputs = document.querySelectorAll(
+    "#characterNameList > ul > li > input"
+  );
+
+  data.slots.forEach((slot) => {
+    if (slot.id.toString() === data.activeSlot.id.toString()) {
+      slot.characters.one = characterNameInputs[0].value;
+      slot.characters.two = characterNameInputs[1].value;
+      slot.characters.three = characterNameInputs[2].value;
+      slot.characters.four = characterNameInputs[3].value;
+      slot.characters.five = characterNameInputs[4].value;
+      slot.characters.six = characterNameInputs[5].value;
+      slot.characters.seven = characterNameInputs[6].value;
+      slot.characters.eight = characterNameInputs[7].value;
+      slot.characters.nine = characterNameInputs[8].value;
+      slot.characters.ten = characterNameInputs[9].value;
+      data.activeSlot.characters = slot.characters;
+      writeJsonFileSync(JSON.stringify(data));
+      updateUI();
     }
   });
 }
